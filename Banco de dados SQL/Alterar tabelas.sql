ea65b4e13783f899fa0242a5dd55a9bdce8a1db0
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-08-31 16:01:47 -0300

    Update Alterar tabelas.sql

--- Banco de dados SQL/Alterar tabelas.sql
+++ Banco de dados SQL/Alterar tabelas.sql
@@ -2,5 +2,42 @@ ALTER TABLE nome_da_tabela
 ADD nome_da_coluna tipodacoluna;
 #Esse comando adicina uma coluna na tabela
 #Exemplo:
-ALTER TABLE Customers
-ADD Email varchar(255);
+ALTER TABLE clientes
+ADD Telefone varchar(255);
+#Esse comando adiciona a coluna telefone na tabela clientes
+
+#Use esse comando para excluir uma coluna de uma tabela
+ALTER TABLE nome_da_tabela
+DROP COLUMN nome_da_coluna;
+#Exemplo
+ALTER TABLE clientes
+DROP COLUMN Telefone;
+#Esse comando apaga a coluna telefone da tabela clientes
+
+
+#Use esse comando para renomear uma coluna da tabela  
+ALTER TABLE nome_da_tabela
+RENAME COLUMN antigo_nome to novo_nome;
+#Exemplo
+ALTER TABLE clientes
+RENAME COLUMN Telefone to Telefones;
+#Esse comando renomeia a coluna telefone para telefones
+
+
+#Esse comando altera o tipo de dados de uma coluna em uma tabela
+ALTER TABLE nome_da_tabela
+ALTER COLUMN nome_da_coluna data_tipo;
+#Caso esse comando não funcione use:
+ALTER TABLE nome_da_tabela
+MODIFY COLUMN nome_da_coluna data_tipo;
+
+
+
+
+
+
+
+
+
+
+
