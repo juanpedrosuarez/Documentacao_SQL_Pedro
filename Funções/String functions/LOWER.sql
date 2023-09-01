389fea5b0b1d3af85c29b26fa92be0194194636d
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 18:39:59 -0300

    Create LOWER.sql

---
+++ Funções/String functions/LOWER.sql
@@ -0,0 +1,8 @@
+A função LOWER() converte uma string em minúsculas.
+
+Nota: A função LCASE() é igual à função LOWER().
+LOWER(text)
+
+Converta o texto em "NOMECLIENTE" para letras minúsculas:
+
+SELECT LOWER(NOMECLIENTE) AS NOMELOWER FROM TABELACLIENTE;
