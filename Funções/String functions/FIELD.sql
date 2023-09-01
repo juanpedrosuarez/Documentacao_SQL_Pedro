38ed314c0db61c41a447238a6080a2582682725f
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 17:51:52 -0300

    Create FIELD.sql

---
+++ Funções/String functions/FIELD.sql
@@ -0,0 +1,9 @@
+Retorne a posição do índice "q" na lista de strings:
+
+A função FIELD() retorna a posição do índice de um valor em uma lista de valores.
+FIELD(value, val1, val2, val3, ...)
+
+Retorne a posição do índice "a" na lista de strings:
+SELECT FIELD("a", "p", "r", "a");
+
+Funciona com numeros
