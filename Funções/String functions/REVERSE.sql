1a7e74897cb63b65225b3eca45d5a5d67334754b
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 19:10:22 -0300

    Create REVERSE.sql

---
+++ Funções/String functions/REVERSE.sql
@@ -0,0 +1,8 @@
+Inverta uma string:
+SELECT REVERSE("SQL Tutorial");
+A função REVERSE() inverte uma string e retorna o resultado.
+REVERSE(string)
+
+Inverta o texto em CLIENTES:
+SELECT REVERSE(CLIENTE)
+FROM CLIENTES;
