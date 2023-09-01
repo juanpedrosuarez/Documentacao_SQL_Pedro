d1576a246264c14b4031c4fc18c7fd08ee689be1
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 15:59:45 -0300

    Create ASC.sql

---
+++ Keywords/ASC.sql
@@ -0,0 +1,5 @@
+O ASCcomando é usado para classificar os dados retornados em ordem crescente.
+
+A seguinte instrução SQL seleciona todas as colunas da tabela "clientes", classificadas pela coluna "NomeDoCliente":
+SELECT * FROM clientes
+ORDER BY NomeDoCliente ASC;
