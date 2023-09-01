00822df303674952a75690ae9fa63ed058f137b9
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-08-31 21:18:03 -0300

    Create Min e Max.sql

---
+++ Sintaxe SQL/Min e Max.sql
@@ -0,0 +1,20 @@
+A Função MAX()função retorna o maior valor da coluna selecionada.
+
+SELECT MAX(column_name)
+FROM table_name
+WHERE condition;
+
+Exemplo:
+SELECT MAX(preco)
+FROM produtos
+  
+
+A Função MIN()função retorna o menor valor da coluna selecionada.
+
+SELECT MIN(column_name)
+FROM table_name
+WHERE condition;
+
+SELECT MIN(preco)
+FROM produtos
+
