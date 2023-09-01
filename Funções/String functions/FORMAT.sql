0d17047651be74f8efd481cee5eefbc08a067511
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 18:05:47 -0300

    Create FORMAT.sql

---
+++ Funções/String functions/FORMAT.sql
@@ -0,0 +1,7 @@
+A função FORMAT() formata um número para um formato como "#,###,###.##", arredondado
+para um número especificado de casas decimais e retorna o resultado como uma string.
+  
+FORMAT(NUMERO, CASASDECIMAIS)
+  
+Formate o número como "#,###,###.##" (e arredonde com duas casas decimais):
+SELECT FORMAT(250500.5634, 2);
