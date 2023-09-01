f83a52399dc08bae2b7ed2c7e9f2dfdce9803ee5
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 16:45:22 -0300

    Update CASE.sql

--- Keywords/CASE.sql
+++ Keywords/CASE.sql
@@ -1,1 +1,10 @@
+O CASE é usado para criar saídas diferentes com base nas condições.
 
+O SQL a seguir passa por diversas condições e retorna um valor quando a condição especificada é atendida:
+SELECT PEDIDOID, QUANTIDADE
+CASE
+    WHEN QUANTIDADE > 30 THEN 'maior que 30'
+    WHEN QUANTIDADE = 30 THEN 'igual a 30'
+    ELSE 'menor que 30'
+END
+FROM DETALHES;
