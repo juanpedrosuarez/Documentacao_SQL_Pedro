858cddf329ab8ea09910beb4120e3f1f6c03c3c1
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 16:50:53 -0300

    Update TOP.sql

--- Keywords/TOP.sql
+++ Keywords/TOP.sql
@@ -1,1 +1,5 @@
+O comando LIMIT, SELECT TOP ou ROWNUMé usado para especificar o número de registros a serem retornados.
 
+A instrução SQL a seguir seleciona os três primeiros registros da tabela "Clientes" (SQL SERVER):
+
+SELECT TOP 3 * FROM clientes;
