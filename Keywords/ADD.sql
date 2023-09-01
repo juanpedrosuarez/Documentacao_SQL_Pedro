765ee2c12f246e7c1dddbfd5d14bb0ce6cd5bc75
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 04:18:55 -0300

    Update and rename ADD to ADD.sql

---
+++ Keywords/ADD.sql
@@ -0,0 +1,5 @@
+O ADDcomando é usado para adicionar uma coluna em uma tabela existente.
+
+O SQL a seguir adiciona uma coluna "E-mail" à tabela "clientes":
+ALTER TABLE clientes
+ADD Email varchar(255);
