9354c5592bd60501482ea078437fbccfffa8d71f
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 19:08:54 -0300

    Create REPLACE.sql

---
+++ Funções/String functions/REPLACE.sql
@@ -0,0 +1,6 @@
+Substitua “SQL” por “HTML”:
+SELECT REPLACE("SQL", "SQLX", "HTML");
+
+A função REPLACE() substitui todas as ocorrências de uma substring dentro de uma string por uma nova substring.
+Nota: Esta função realiza uma substituição que diferencia maiúsculas de minúsculas.
+REPLACE(string, from_string, new_string)
