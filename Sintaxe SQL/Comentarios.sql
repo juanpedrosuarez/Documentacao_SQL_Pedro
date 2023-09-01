649013f83681133d22f19e3f4d1cf4b1a3d718c2
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 04:11:12 -0300

    Create Comentarios.sql

---
+++ Sintaxe SQL/Comentarios.sql
@@ -0,0 +1,8 @@
+Qualquer texto entre -- e o final da linha será ignorado (não será executado).
+--aaaa
+SELECT * FROM dados;
+Comentários de várias linhas começam /*e terminam com */
+/*ISS18
+  JUAN
+  ISS18*/
+SELECT * FROM dados;
