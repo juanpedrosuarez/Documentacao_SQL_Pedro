410edfe420f6a36d1ea263f0102f3f7f45229212
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 16:46:27 -0300

    Update CHECK.sql

--- Keywords/CHECK.sql
+++ Keywords/CHECK.sql
@@ -1,1 +1,9 @@
 
+A CHECK limita o valor que pode ser colocado em uma coluna.
+
+O SQL a seguir cria uma restrição CHECK na coluna "Idade" quando a tabela "Pessoas" é criada.
+A restrição CHECK garante que você não pode ter nenhuma pessoa com menos de 18 anos:
+CREATE TABLE PESSOAS(
+    IDADE int,
+    CHECK (IDADE>=18)
+);
