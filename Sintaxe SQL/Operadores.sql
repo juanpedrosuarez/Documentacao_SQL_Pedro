f46a95df791fc56af02ac27659e818c841816499
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 04:12:39 -0300

    Create Operadores.sql

---
+++ Sintaxe SQL/Operadores.sql
@@ -0,0 +1,25 @@
+Operadores de comparação: são usados para comparar valores e retornar um valor booleano. Alguns exemplos são:
+== (igual a)
+!= (diferente de)
+< (menor que)
+> (maior que)
+<= (menor ou igual a)
+>= (maior ou igual a)
+Operadores lógicos: são usados para combinar duas ou mais expressões lógicas. Alguns exemplos são:
+AND (e)
+OR (ou)
+NOT (não)
+Operadores aritméticos: são usados para realizar operações matemáticas em valores. Alguns exemplos são:
++ (soma)
+- (subtração)
+* (multiplicação)
+/ (divisão)
+% (módulo)
+Operadores de concatenação: são usados para concatenar duas ou mais strings. O operador de concatenação é o ponto (.).
+Operadores de bit: são usados para realizar operações em bits. Alguns exemplos são:
+& (and bit a bit)
+| (or bit a bit)
+^ (xor bit a bit)
+~ (not bit a bit)
+<< (shift left)
+>> (shift right)
