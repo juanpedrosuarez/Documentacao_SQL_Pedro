ab637a0f4c2f915433d80a1776b38945a7899b3d
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 18:51:57 -0300

    Create POSITION.sql

---
+++ Funções/String functions/POSITION.sql
@@ -0,0 +1,6 @@
+Procure por "P" na string "pjuan.com" e retorne a posição:
+SELECT POSITION('3' IN 'pjuan.com')
+
+Procure por "a" na coluna NOMECLIENTE e retorne a posição:
+
+SELECT POSITION('a' IN NOMECLIENTE) FROM CLIENTES
