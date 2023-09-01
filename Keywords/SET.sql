d2e929b721044c41058dc603413369dc9409a28b
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 16:49:54 -0300

    Update SET.sql

--- Keywords/SET.sql
+++ Keywords/SET.sql
@@ -1,1 +1,12 @@
+O SET é usado com UPDATE para especificar quais colunas e valores devem ser atualizados em uma tabela.
 
+O SQL a seguir atualiza o primeiro cliente (ID = 1) com um novo NOME e uma nova CIDADE:
+
+UPDATE CLIENTE
+SET NOME = 'Juan', City= 'São Paulo'
+WHERE ID = 1;
+
+O SQL a seguir atualizará o campo "NOME" para "Juan" para todos os registros onde o país é "Brasil":
+UPDATE Customers
+SET ContactName='Juan'
+WHERE Country='Brasil';
