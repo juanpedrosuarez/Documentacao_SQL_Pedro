632c4b7d908d05ad511c8a2150c7074e4d6ddfcc
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 17:41:06 -0300

    Create CHAR LENGHT.sql

---
+++ Funções/String functions/CHAR LENGHT.sql
@@ -0,0 +1,7 @@
+Retorne o comprimento da string:
+  
+SELECT CHAR_LENGTH("SQL") AS Length;
+
+Retorne o comprimento do texto na coluna "CLIENTESNOME":
+SELECT CHAR_LENGTH(CLIENTESNOME) AS NOME
+FROM CLIENTES;
