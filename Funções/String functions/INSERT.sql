9170b1e28dc082b1bba3116530f5996e5b26f0d2
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 18:13:39 -0300

    Create INSERT.sql

---
+++ Funções/String functions/INSERT.sql
@@ -0,0 +1,6 @@
+A função INSERT() insere uma string dentro de uma string na posição especificada e para um certo número de caracteres.
+INSERT(string, posição, numero, string2)
+
+Insira a string "Exemplo" na string "W3Schools.com". Substitua os primeiros nove caracteres:
+SELECT INSERT("Juan.com", 1, 4, "Pedro");
+ficaria Pedro.com
