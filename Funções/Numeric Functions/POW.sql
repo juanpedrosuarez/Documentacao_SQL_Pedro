804876df7dc4440175464bdf2104bcf1fdf20f0d
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:48:12 -0300

    Create POW.sql

---
+++ Funções/Numeric Functions/POW.sql
@@ -0,0 +1,5 @@
+A função POW() retorna o valor de um número elevado à potência de outro número.
+Nota: Esta função é igual à função POWER() 
+
+Retorne 4 elevado à segunda potência:
+SELECT POW(4, 2);
