39ebee00c77fd7bf2df09cb62a502d9cd615913a
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:34:29 -0300

    Create EXP.sql

---
+++ Funções/Numeric Functions/EXP.sql
@@ -0,0 +1,9 @@
+A função EXP() retorna e elevado à potência do número especificado.
+A constante e (2,718281...), é a base dos logaritmos naturais.
+EXP(number)
+  
+Retorne e elevado à potência de 1:
+SELECT EXP(1);
+
+Retorne e elevado à potência de 2:
+SELECT EXP(2);
