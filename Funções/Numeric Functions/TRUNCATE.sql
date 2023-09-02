dd86ab66861431025f743dc914ef84776d2a67ff
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:54:27 -0300

    Create TRUNCATE.sql

---
+++ Funções/Numeric Functions/TRUNCATE.sql
@@ -0,0 +1,5 @@
+A função TRUNCATE() trunca um número para o número especificado de casas decimais.
+TRUNCATE(numero, casasdecimais)
+  
+Retorne um número truncado com 2 casas decimais:
+SELECT TRUNCATE(135.375, 2);
