bfcf7db01ee83a7bb0570347e7fb2ee2a7075f7a
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:29:28 -0300

    Create CELING.sql

---
+++ Funções/Numeric Functions/CELING.sql
@@ -0,0 +1,8 @@
+Retorne o menor valor inteiro maior ou igual a 25,75:
+
+SELECT CEILING(25.75);
+
+A função CEILING() retorna o menor valor inteiro maior ou igual a um número.
+
+Nota: Esta função é igual à função CEIL()
+CEILING(number)
