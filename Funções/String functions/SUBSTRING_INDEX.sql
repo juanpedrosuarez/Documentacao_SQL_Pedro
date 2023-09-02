629b528e213202bfd4ffc84e50271fbbb5549196
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 21:37:50 -0300

    Create SUBSTRING_INDEX.sql

---
+++ Funções/String functions/SUBSTRING_INDEX.sql
@@ -0,0 +1,5 @@
+Retorna uma substring de uma string antes que ocorra um número especificado de delimitadores:
+SUBSTRING_INDEX(string, delimiter, number)
+
+Retorna uma substring de uma string antes que ocorra um número especificado de delimitadores:
+SELECT SUBSTRING_INDEX("aaa", ".", 1);
