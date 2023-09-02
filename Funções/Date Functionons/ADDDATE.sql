42142599413e4ee7f3d01917a9012cf1cb1ff7f0
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:56:50 -0300

    Update ADDDATE.sql

--- Funções/Date Functionons/ADDDATE.sql
+++ Funções/Date Functionons/ADDDATE.sql
@@ -1,1 +1,11 @@
+A função ADDDATE() adiciona um intervalo de hora/data a uma data e depois retorna a data.
+ADDDATE(date, days)
+  
+Adicione 10 dias a uma data e retorne a data:
+SELECT ADDDATE("2002-05-24", INTERVAL 10 DAY);
 
+Adicione 15 minutos a uma data e retorne a data:
+SELECT ADDDATE("2002-05-24", INTERVAL 10 MINUTE);
+
+Subtraia 3 horas a uma data e retorne a data:
+SELECT ADDDATE("2002-05-24", INTERVAL 10 HOUR);
