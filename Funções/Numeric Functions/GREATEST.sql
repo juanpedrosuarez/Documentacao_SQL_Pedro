c754a010411a151d019fbdd606169ebb38b7f1fe
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:38:13 -0300

    Create GREATEST.sql

---
+++ Funções/Numeric Functions/GREATEST.sql
@@ -0,0 +1,5 @@
+Retorne o maior valor da lista de argumentos:
+SELECT GREATEST(3, 12, 34, 8, 25);
+
+A função GREATEST() retorna o maior valor da lista de argumentos.
+GREATEST(arg1, arg2, arg3, ...)
