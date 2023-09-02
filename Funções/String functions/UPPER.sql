38170753e183737b5c9b793a979ce2e8b3a859e3
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 21:41:44 -0300

    Create UPPER.sql

---
+++ Funções/String functions/UPPER.sql
@@ -0,0 +1,10 @@
+Converta o texto para maiúsculas:
+SELECT UPPER("SQL");
+
+A função UPPER() converte uma string em maiúsculas.
+Nota: Esta função é igual à função UCASE() .
+UPPER(text)
+  
+Converta o texto em "CustomerName" para letras maiúsculas:
+SELECT UPPER(clientesnome)
+FROM clientes;
