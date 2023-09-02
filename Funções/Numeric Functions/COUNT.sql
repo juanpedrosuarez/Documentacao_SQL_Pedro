3b22f83b97a761fe30f6bb1c85180996b0709adb
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:31:23 -0300

    Create COUNT.sql

---
+++ Funções/Numeric Functions/COUNT.sql
@@ -0,0 +1,7 @@
+Retorne a quantidade de produtos na tabela "Produtos":
+SELECT COUNT(produtoID) numeroDeProdutos FROM produtos;
+
+A função COUNT() retorna o número de registros retornados por uma consulta selecionada.
+
+Nota: Valores NULL não são contados.
+COUNT(expression)
