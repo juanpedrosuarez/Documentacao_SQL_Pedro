80280f7c97eb2a3671f1044304578a8f0da5da58
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:35:28 -0300

    Create FLOOR.sql

---
+++ Funções/Numeric Functions/FLOOR.sql
@@ -0,0 +1,7 @@
+A função FLOOR() retorna o maior valor inteiro menor ou igual a um número.
+
+Nota: observe também as funções ROUND() , CEIL() , CEILING() , TRUNCATE() e DIV .
+FLOOR(number)
+  
+Retorne o maior valor inteiro menor ou igual a 25,75:
+SELECT FLOOR(25.75);
