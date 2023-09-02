dde81a3d493ee589ab49f073e378b473014ce98c
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 21:17:18 -0300

    Create RIGHT.sql

---
+++ Funções/String functions/RIGHT.sql
@@ -0,0 +1,7 @@
+Extraia 4 caracteres de uma string (começando da direita):
+SELECT RIGHT("SQL", 2) AS estrair;
+
+A função RIGHT() extrai vários caracteres de uma string (começando da direita).
+
+Dica: observe também a função LEFT()
+RIGHT(string, numerodechars)
