28f272a5a33099129edded0973bf1432401c1b12
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-01 22:28:44 -0300

    Create CEIL.sql

---
+++ Funções/Numeric Functions/CEIL.sql
@@ -0,0 +1,6 @@
+Retorne o menor valor inteiro maior ou igual a 25,75:
+SELECT CEIL(25.75);
+
+A função CEIL() retorna o menor valor inteiro maior ou igual a um número.
+Nota: Esta função é igual à função CEILING()
+CEIL(number)
