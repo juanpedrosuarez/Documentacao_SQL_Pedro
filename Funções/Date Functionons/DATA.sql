00fe50cfe73f4fbdeb2f0c06f9bf640f67ccd400
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-02 22:13:28 -0300

    Create DATA.sql

---
+++ Funções/Date Functionons/DATA.sql
@@ -0,0 +1,8 @@
+A função DATE() extrai a parte da data de uma expressão de data e hora.
+DATE(expression)
+
+Extraia a parte da data:
+SELECT DATE("2002-06-15");
+
+Extraia a parte da data:
+SELECT DATE(datapedido) FROM pedidos;
