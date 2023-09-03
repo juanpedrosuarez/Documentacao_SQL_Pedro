feb5ce99cace6d62562067bd1c697d07cf631867
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-02 22:14:59 -0300

    Create DATEDIFF.sql

---
+++ Funções/Date Functionons/DATEDIFF.sql
@@ -0,0 +1,5 @@
+A função DATEDIFF() retorna o número de dias entre dois valores de data.
+DATEDIFF(date1, date2)
+
+Retorne o número de dias entre dois valores de data:
+SELECT DATEDIFF("2002-06-25", "2002-06-15");
