8840051b3ea9edde2ec78da118e387c6d1ff446e
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-04 23:05:51 -0300

    Create IF.sql

---
+++ Funções/Funções avançadas/IF.sql
@@ -0,0 +1,5 @@
+A função IF() retorna um valor se uma condição for VERDADEIRA, ou outro valor se uma condição for FALSA.
+IF(condition, value_if_true, value_if_false)
+
+Retorne "SIM" se a condição for VERDADEIRA ou "NÃO" se a condição for FALSA:
+SELECT IF(500<1000, "YES", "NO");
