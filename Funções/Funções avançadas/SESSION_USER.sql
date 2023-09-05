13faa74775f902d0a48093106b0bf70f6640d843
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-04 23:10:32 -0300

    Create SESSION_USER.sql

---
+++ Funções/Funções avançadas/SESSION_USER.sql
@@ -0,0 +1,8 @@
+A função SESSION_USER() retorna o nome de usuário atual e o nome do host para a conexão MySQL.
+
+Nota: Esta função é igual às funções SYSTEM_USER() e USER() 
+
+SESSION_USER()
+
+Retorne o nome de usuário e o nome do host atuais para a conexão MySQL:
+SELECT SESSION_USER();
