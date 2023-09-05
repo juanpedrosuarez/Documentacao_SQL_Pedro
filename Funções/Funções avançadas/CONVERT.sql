2d1f7b9e438f163c0cef4a7ea5558d1eaf2a006a
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-04 23:02:39 -0300

    Create CONVERT.sql

---
+++ Funções/Funções avançadas/CONVERT.sql
@@ -0,0 +1,7 @@
+A função CONVERT() converte um valor no tipo de dados ou conjunto de caracteres especificado.
+
+Dica: observe também a função CAST() .
+CONVERT(value, type)
+
+Converta um valor em um tipo de dados DATE:
+SELECT CONVERT("2017-08-29", DATE);
