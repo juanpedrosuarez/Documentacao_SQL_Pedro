20e90aef019347c3713050f4490ec76cfa9555e6
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-04 22:59:14 -0300

    Create  CAST.sql

---
+++ Funções/Funções avançadas/CAST.sql
@@ -0,0 +1,7 @@
+A função CAST() converte um valor (de qualquer tipo) no tipo de dados especificado.
+
+Dica: Veja também a função CONVERT() .
+CAST(value AS datatype)
+
+Converta um valor em um tipo de dados DATE:
+SELECT CAST("2017-08-29" AS DATE);
