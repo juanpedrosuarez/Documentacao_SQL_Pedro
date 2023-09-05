d179b10595ed3564357aeaf40179f59a665406c3
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-04 23:07:53 -0300

    Create ISNULL.sql

---
+++ Funções/Funções avançadas/ISNULL.sql
@@ -0,0 +1,7 @@
+A função ISNULL() retorna 1 ou 0 dependendo se uma expressão é NULL.
+
+Se a expressão for NULL, esta função retornará 1. Caso contrário, retornará 0.
+ISNULL(expression)
+  
+Teste se uma expressão é NULL:
+SELECT ISNULL(NULL);
