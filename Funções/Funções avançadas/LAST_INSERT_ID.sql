5696ac51f049adc37f5bb54c5df5ceb8f955e496
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-09-04 23:08:50 -0300

    Create LAST_INSERT_ID.sql

---
+++ Funções/Funções avançadas/LAST_INSERT_ID.sql
@@ -0,0 +1,5 @@
+A função LAST_INSERT_ID() retorna o ID AUTO_INCREMENT da última linha que foi inserida ou atualizada em uma tabela.
+LAST_INSERT_ID(expression)
+
+Retorna o id AUTO_INCREMENT da última linha que foi inserida ou atualizada em uma tabela:
+SELECT LAST_INSERT_ID();
